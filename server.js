f76efcbd661121fdf2617c981e8b20cdb2d5c503
Nandan S <thenandansrinivas@outlook.com>
2024-08-07 20:01:12 +0530

    fist commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json('Hello World!');
 });
 
 app.listen(3000).then(() => console.log('Server running on port 3000'));
